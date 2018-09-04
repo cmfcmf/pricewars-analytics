9622544a0339ab7e9308eff485e7bf5aaa759b58
Carsten Walther <carsten_walther@gmx.de>
2018-09-04 20:07:03 +0200

    use quantity instead of amount

--- common/src/main/scala/de/hpi/epic/pricewars/logging/producer/Order.scala
+++ common/src/main/scala/de/hpi/epic/pricewars/logging/producer/Order.scala
@@ -1,8 +1,8 @@
 package de.hpi.epic.pricewars.logging.producer
 
-import de.hpi.epic.pricewars.logging.base.{AmountEntry, MerchantIDEntry, TimestampEntry}
+import de.hpi.epic.pricewars.logging.base.{MerchantIDEntry, TimestampEntry}
 import de.hpi.epic.pricewars.types._
 
 case class Order(billing_amount: Currency, product_id: ID, name: Name, unit_price: Currency,
-                 amount: Amount, merchant_id: Token, timestamp: Timestamp)
-  extends MerchantIDEntry with AmountEntry with TimestampEntry
+                 quantity: Quantity, merchant_id: Token, timestamp: Timestamp)
+  extends MerchantIDEntry with TimestampEntry
